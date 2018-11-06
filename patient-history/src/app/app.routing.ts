cea2e397dc6bdf373bad005a39c63e046c467bcc
Tân Võ Minh <vominhtan1988@gmail.com>
2018-11-06 11:47:21 +0700

    UPDATE ROUTING

--- patient-history/src/app/app.routing.ts
+++ patient-history/src/app/app.routing.ts
@@ -1,16 +1,13 @@
 import {Routes} from "@angular/router";
 import {UiExampleComponent} from "./components/ui-example/ui-example.component";
+import {PatientList} from "./components/patient-list.component";
 
 export const appRoutes: Routes = [
   { path: 'ui-example', component: UiExampleComponent },
-  // {
-  //   path: 'heroes',
-  //   component: HeroListComponent,
-  //   data: { title: 'Heroes List' }
-  // },
   { path: '',
-    redirectTo: '/ui-example',
+    redirectTo: '/patient-list',
     pathMatch: 'full'
   },
+  { path: 'patient-list', component: PatientList },
   // { path: '**', component: PageNotFoundComponent }
 ];
